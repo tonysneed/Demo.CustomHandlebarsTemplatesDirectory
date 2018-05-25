2fcfef94d1e16ea76a5268c69950bc6a8b0c4b4c
Anthony Sneed <tony@tonysneed.com>
2018-05-25 15:35:28 -0500

    Add project files.

---
+++ CustomHandlebarsTemplatesDirectory.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace CustomHandlebarsTemplatesDirectory.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
